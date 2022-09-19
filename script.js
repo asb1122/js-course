ecdb1db736465da76669548652eb12aa33b00271
Artsiom <asb1122@yandex.ru>
2022-09-19 23:17:51 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const person = `Alex`;
+
+const bool = false;
+
+const obj = {
+    name: 'alex',
+    age: 53,
+    isMarried: false,
+    abc: {
+        def: {
+
+        },
+        ref: {
+
+        },
+        index: []
+    }
+}
+
+obj.job = 'it';
+
+console.log(obj.job)
+
+
+let incr = 10,
+    decr = 10;
+
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
